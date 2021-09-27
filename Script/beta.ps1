0a3298fcb68985e6ea3e3d5f4d4aad99857e4c1e
Nelson Orellana <55712980+Nels2@users.noreply.github.com>
2021-09-27 15:43:23 -0500

    Update beta.ps1
    update for local changes

--- Script/beta.ps1
+++ Script/beta.ps1
@@ -1,30 +1,31 @@
-$FileName = "\\path\to\connectionsCurrent.ppsx"
+$FileName = "path\to\connectionsCurrent.ppsx"
 $FileNameTwo = "path\to\connectionsCurrent\FakeLocalDirectory\connectionsCurrent.ppsx"
 $LocalDir = "path\to\connectionsCurrent\FakeLocalDirectory\"
 $FileTime = Get-Date
-{"----- You are using connectionsCurrent, a small network-copy script made for CBT. -----"}
+Write-Output "----- You are using connectionsCurrent, a small network-copy script made for CBT. -----"
+Write-Output "-----                                                                             -----"
+Write-Output "-----                                                                             -----"
+Write-Output "-----                           Made by: nelson orellana                          -----"
+Write-Output "-----                                                                             -----"
+Write-Output "-----                                                                             -----"
 # endless loop
 for () {
     $file = Get-Item $FileName
     if ($FileTime -ne $file.LastWriteTime) {
-        try {
-            Get-Process powerpnt* | Stop-Process
-            Stop-Process -name POWERPNT -Force
-        }
-        catch {
-            {"!WARNING!: PowerPoint is not currently running."}
-        }
+        Get-Process powerpnt*
+        Stop-Process -name POWERPNT -Force -ErrorAction Ignore
         if((Get-FileHash $fileNameTwo).hash  -ne (Get-FileHash $fileName).hash)
-            {"!NOTICE!: files are different, copying.."}
+            {"files are different, copying in new one to local directory.."}
             #Check destination path
             if (Test-Path $FileName)
                 {
                 #then copy
-                robocopy $FileName $LocalDir /MIR /Z /E /fft 
+                robocopy $FileName $LocalDir /MIR /Z /E /fft
                 }
-            $NewFile = "path\to\connectionsCurrent\FakeLocalDirectory\connectionsCurrent.ppsx"
-            Invoke-Item $NewFile
+            $NewFile = $FileNameTwo
+            Start-Process $NewFile
+            Write-Output "The directory name is not invalid."
     }
     $FileTime = $file.LastWriteTime
-    Start-Sleep 10
+    Start-Sleep -Seconds 5
 }

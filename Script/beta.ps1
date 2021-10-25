c9e6bc8fa195c466ea5571a90e1b8f335f5118f8
Nelson Orellana <55712980+Nels2@users.noreply.github.com>
2021-10-25 13:21:31 -0500

    Update beta.ps1

--- Script/beta.ps1
+++ Script/beta.ps1
@@ -2,7 +2,6 @@ $FileNameTwo = "C:\connectionsCurrent\FakeLocalDirectory\connectionsCurrent.pps"
 $LocalDir = "C:\connectionsCurrent\FakeLocalDirectory\" # local directory where the network file will be copied to.
 $FileTime = Get-Date
 Write-Output ">>You are using connectionsCurrent, a small network-copy script made for CBT."
-Write-Output "|  "
 Write-Output ">>Made By: Nelson Orellana in Sept. 2021"
 Write-Output ">>Social: Nels2/GitHub"
 Write-Output "|  "
@@ -13,13 +12,13 @@ for () {
     # The [1] part at the end tells the command to return only the result at index 1.
     # Indexes start with 0, which means specifying index 1 will show the result at the second position.
     $dauser= ((Get-WMIObject -ClassName Win32_ComputerSystem).Username).Split('\')[1]
-    $FileName = "C:\Users\$dauser\OneDrive_Location\Connection\connectionsCurrent.pps" #Network location / network file
+    $FileName = "C:\Users\$dauser\OneDrive - Century Business Technologies Inc\Company Documents\Connection\connectionsCurrent.pps" #Network location / network file
     $file = Get-Item -force $FileName
     if ($FileTime -ne $file.LastWriteTime) {
         Get-Process powerpnt*  
         Stop-Process -name POWERPNT -Force -ErrorAction Ignore
         if((Get-FileHash $FileNameTwo).hash  -ne (Get-FileHash $FileName).hash)
-            {">>Files are different, copying in new one to local directory! .."}
+            {">>Files are different, copying in new one to local directory! ..."}
             #Check destination path
             if (Test-Path $FileName)
                 {
@@ -30,7 +29,7 @@ for () {
             $NewFile = $FileNameTwo
             $daet = Get-Date -DisplayHint Date #misspelled date on purpose.
             $filed = Split-Path -Path $Newfile -Leaf -Resolve #splitting up file path so i can make output look a bit more neater.
-            Write-Output ">>Launching $daet's $filed! .."
+            Write-Output ">>Launching $daet's $filed! ..."
             Start-Process $NewFile
     }
     $FileTime = $file.LastWriteTime

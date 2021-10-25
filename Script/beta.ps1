5f11fd592bf102137d7318911315e1dac2e1af5d
Nelson Orellana <55712980+Nels2@users.noreply.github.com>
2021-10-25 13:04:21 -0500

    Update beta.ps1
    revert back to old method after doing more research and finding a more simplistic approach.

--- Script/beta.ps1
+++ Script/beta.ps1
@@ -1,4 +1,4 @@
-$FileNameTwo = "C:\connectionsCurrent\FakeLocalDirectory\connectionsCurrent.pptx" # local file that will have its' hashes compared to the new one.
+$FileNameTwo = "C:\connectionsCurrent\FakeLocalDirectory\connectionsCurrent.pps" # local file that will have its' hashes compared to the new one.
 $LocalDir = "C:\connectionsCurrent\FakeLocalDirectory\" # local directory where the network file will be copied to.
 $FileTime = Get-Date
 Write-Output "----- You are using connectionsCurrent, a small network-copy script made for CBT. -----"
@@ -13,7 +13,7 @@ for () {
     # The [1] part at the end tells the command to return only the result at index 1.
     # Indexes start with 0, which means specifying index 1 will show the result at the second position.
     $dauser= ((Get-WMIObject -ClassName Win32_ComputerSystem).Username).Split('\')[1]
-    $FileName = "C:\Users\$dauser\OneDrive_Location\Connection\connectionsCurrent.pptx" #Network location / network file
+    $FileName = "C:\Users\$dauser\OneDrive_Location\Connection\connectionsCurrent.pps" #Network location / network file
     $file = Get-Item -force $FileName
     if ($FileTime -ne $file.LastWriteTime) {
         Get-Process powerpnt*  
@@ -27,11 +27,8 @@ for () {
                 xcopy $FileName $LocalDir /E /Z /F /R /y
                 }
                 #now to launch the powerpoint and play the slideshow
-            $ppt = $FileNameTwo
-            $app = New-Object -ComObject powerpoint.application
-                        $pres = $app.Presentations.open($ppt)
-                        $pres.SlideShowSettings.Run()
-                        #$pres.visible = "msoTrue" # this for some reason doesn't work but I', curious about it so, leaving it in.
+            $NewFile = $FileNameTwo
+            Start-Process $NewFile
     }
     $FileTime = $file.LastWriteTime
     Start-Sleep -Seconds 5

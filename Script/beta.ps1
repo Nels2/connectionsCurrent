fe521f5810a36df4397caf35d62805d088bc03fa
Nelson Orellana <55712980+Nels2@users.noreply.github.com>
2021-10-25 13:17:32 -0500

    Update beta.ps1
    update how output will look

--- Script/beta.ps1
+++ Script/beta.ps1
@@ -1,12 +1,12 @@
 $FileNameTwo = "C:\connectionsCurrent\FakeLocalDirectory\connectionsCurrent.pps" # local file that will have its' hashes compared to the new one.
 $LocalDir = "C:\connectionsCurrent\FakeLocalDirectory\" # local directory where the network file will be copied to.
 $FileTime = Get-Date
-Write-Output "----- You are using connectionsCurrent, a small network-copy script made for CBT. -----"
-Write-Output "-----                                                                             -----"
-Write-Output "-----                                                                             -----"
-Write-Output "-----                           Made by: nelson orellana                          -----"
-Write-Output "-----                                                                             -----"
-Write-Output "-----                                                                             -----"
+Write-Output ">>You are using connectionsCurrent, a small network-copy script made for CBT."
+Write-Output "|  "
+Write-Output ">>Made By: Nelson Orellana in Sept. 2021"
+Write-Output ">>Social: Nels2/GitHub"
+Write-Output "|  "
+Write-Output "|  "
 # endless loop
 for () {
     # The command below will split the username property into two using the backslash '\' character as the delimiter.
@@ -19,7 +19,7 @@ for () {
         Get-Process powerpnt*  
         Stop-Process -name POWERPNT -Force -ErrorAction Ignore
         if((Get-FileHash $FileNameTwo).hash  -ne (Get-FileHash $FileName).hash)
-            {">files are different, copying in new one to local directory! .."}
+            {">>Files are different, copying in new one to local directory! .."}
             #Check destination path
             if (Test-Path $FileName)
                 {
@@ -28,8 +28,9 @@ for () {
                 }
                 #now to launch the powerpoint and play the slideshow
             $NewFile = $FileNameTwo
-            d = Get-Date -DisplayHint Date
-            Write-Output ">Launching $d 's $Newfile.."
+            $daet = Get-Date -DisplayHint Date #misspelled date on purpose.
+            $filed = Split-Path -Path $Newfile -Leaf -Resolve #splitting up file path so i can make output look a bit more neater.
+            Write-Output ">>Launching $daet's $filed! .."
             Start-Process $NewFile
     }
     $FileTime = $file.LastWriteTime

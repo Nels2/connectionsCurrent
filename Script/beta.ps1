2709cc7269b01282a47f3872352df54795067ffe
Nelson Orellana <55712980+Nels2@users.noreply.github.com>
2021-10-27 02:12:01 -0500

    Update beta.ps1
    change method used of opening .pps file to just opening powerpoint with /s (start in slideshow view) to open the .pptx file.

--- Script/beta.ps1
+++ Script/beta.ps1
@@ -1,8 +1,10 @@
-$FileNameTwo = "C:\connectionsCurrent\FakeLocalDirectory\connectionsCurrent.pps" # local file that will have its' hashes compared to the new one.
+$FileNameTwo = "C:\connectionsCurrent\FakeLocalDirectory\connectionsCurrent.pptx" # local file that will have its' hashes compared to the new one.
 $LocalDir = "C:\connectionsCurrent\FakeLocalDirectory\" # local directory where the network file will be copied to.
+$ppt = "c:\Program Files\Microsoft Office\root\Office16\powerpnt.exe"#powerpoint.exe location -- previous method of saving files ass .pps/.ppsx would corrupt the file.
 $FileTime = Get-Date
+Write-Output ">>Today's Date & Current Time: $Filetime"
 Write-Output ">>You are using connectionsCurrent, a small network-copy script made for CBT."
-Write-Output ">>Made By: Nelson Orellana in Sept. 2021"
+Write-Output ">>Made By: Nelson Orellana on Sept. 2021"
 Write-Output ">>Social: Nels2/GitHub"
 Write-Output "|  "
 Write-Output "|  "
@@ -12,7 +14,7 @@ for () {
     # The [1] part at the end tells the command to return only the result at index 1.
     # Indexes start with 0, which means specifying index 1 will show the result at the second position.
     $dauser= ((Get-WMIObject -ClassName Win32_ComputerSystem).Username).Split('\')[1]
-    $FileName = "C:\Users\$dauser\OneDrive - Century Business Technologies Inc\Company Documents\Connection\connectionsCurrent.pps" #Network location / network file
+    $FileName = "C:\Users\$dauser\OneDrive - Century Business Technologies Inc\Company Documents\Connection\connectionsCurrent.pptx" #Network location / network file
     $file = Get-Item -force $FileName
     if ($FileTime -ne $file.LastWriteTime) {
         Get-Process powerpnt*  
@@ -30,7 +32,8 @@ for () {
             $daet = Get-Date -DisplayHint Date #misspelled date on purpose.
             $filed = Split-Path -Path $Newfile -Leaf -Resolve #splitting up file path so i can make output look a bit more neater.
             Write-Output ">>Launching $daet's $filed! ..."
-            Start-Process $NewFile
+            cmd /k "$ppt" /S "$NewFile"#using cmd commmands through powershell to launch powerpoint with $Newfile in slideshow view 
+            #Start-Process $NewFile #will launch just the powerpoint in the Office application.
     }
     $FileTime = $file.LastWriteTime
     Start-Sleep -Seconds 5
